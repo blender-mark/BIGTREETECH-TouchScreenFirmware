0a84fc456443550b9b1ce6c9d7086f9835b901ae
blender-mark <indekeu.marc@gmail.com>
2020-12-10 09:54:45 +0100

    Update Configuration.h
    test baudrate

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -105,7 +105,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 115200
+#define BAUDRATE 250000
 
 /**
  * Default Primary Language (for Touch-Mode only)
